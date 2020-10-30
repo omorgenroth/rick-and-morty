2c15223cc32bb4f415c01741f9ab7e7a857f25eb
Oliver Morgenroth <morgenroth.oliver@gmail.com>
2020-10-30 14:21:26 +0100

    Add Header Component

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+import Logo from './assets/rick_and_morty_logo.png'
+
+export default function Header() {
+  return (
+    <HeaderStyled>
+      <LogoStyled src={Logo} alt="" />
+    </HeaderStyled>
+  )
+}
+
+const HeaderStyled = styled.header`
+  width: 100%;
+  text-align: center;
+`
+
+const LogoStyled = styled.img`
+  filter: drop-shadow(0 4px 4px rgba(0, 0, 0, 0.5));
+`
