c9cf6376ee7e0b717822fa73770832b0d01c9182
Oliver Morgenroth <morgenroth.oliver@gmail.com>
2020-10-31 13:43:17 +0100

    Clean up code

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -33,19 +33,3 @@ const Grid = styled.div`
   display: grid;
   gap: 15px;
 `
-
-/* const Button = styled.button`
-  display: block;
-  padding: 7px 45px;
-  font-size: 18px;
-  font-weight: 400;
-  color: #28302d;
-  border-radius: 10px;
-  border: 2px solid #596f72;
-  background: #a6e255;
-  margin: auto;
-  margin-top: 15px;
-  margin-bottom: 5px;
-  font-family: 'Oswald', sans-serif;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
-` */
